0f0c470cb2c765bbc95f6b59b2c8da43464933ce
Ayush gadre <84783296+Ayushgadre@users.noreply.github.com>
2024-01-21 17:10:19 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,18 @@
+// Styled Components:
+// - Defines styled components for layout purposes using styled-components library.
 import styled from "styled-components";
 
+// Layout component styling
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 `;
 
+// Main component styling
 export const Main = styled.main`
   flex: 1;
-
   display: flex;
   justify-content: center;
-
   background-color: #c3c3c3;
 `;
