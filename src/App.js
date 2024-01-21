f0f3d4632c0fb5f2dd79bd699b63e6585bb79a08
Ayush gadre <84783296+Ayushgadre@users.noreply.github.com>
2024-01-21 17:07:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+// Main App Component:
+// - Manages the overall structure of the application.
 import React, { useState } from "react";
+
+// - Uses React Router for navigation.
 import { HashRouter } from "react-router-dom";
 
+// - Includes a Navbar and Routes component.
 import Navbar from "./components/Navbar/Navbar";
 import Routes from "./Routes";
 
+// - Handles shopping cart state and provides functions to add and remove items.
 import "./assets/global-styles/reset.css";
 import "./assets/global-styles/fonts.css";
 import "./assets/global-styles/global.css";
 import { Layout, Main } from "./styles";
 
 function App() {
+  // Shopping cart state
   const [shoppingCart, setShoppingCart] = useState([]);
 
-  function addItemToCart(item) {
+  // Function to add an item to the shopping cart
+  function addItemToCart(item) { 
     const itemIndex = shoppingCart.findIndex((i) => i.id === item.id);
     if (itemIndex > -1) {
       const newCart = shoppingCart.slice();
@@ -24,25 +32,32 @@ function App() {
     }
   }
 
+  // Function to remove an item from the shopping cart
   function removeItemFromCart(item, removeAll) {
     const itemIndex = shoppingCart.findIndex((i) => i.id === item.id);
     if (itemIndex > -1) {
       const newCart = shoppingCart.slice();
 
+      // Check if removing all or just decreasing quantity
       removeAll ? newCart.splice(itemIndex, 1) : newCart[itemIndex].quantity--;
 
       setShoppingCart(newCart);
     } else {
+      // Log an error if the item is not found in the shopping cart
       console.error(
         "Couldn't remove item from shopping cart: There's no such item in the shopping cart."
       );
     }
   }
 
+  // Render the main structure of the application
   return (
     <Layout>
       <HashRouter basename="/">
+        {/* Display the Navbar with the current cart size */}
         <Navbar cartSize={shoppingCart.length} />
+
+        {/* Main content area with the application routes */}
         <Main>
           <Routes
             shoppingCart={shoppingCart}
@@ -55,4 +70,5 @@ function App() {
   );
 }
 
+// Export the App component
 export default App;
