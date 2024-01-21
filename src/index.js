28f44d7b79dd7e2809a89e35af9494b6f3602b5b
Ayush gadre <84783296+Ayushgadre@users.noreply.github.com>
2024-01-21 17:09:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// Root Rendering:
+// - Uses ReactDOM to render the main App component into the root element.
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Importing the main App component
 import App from "./App";
 
+// Rendering the App component inside React.StrictMode
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // Rendering inside the element with id "root" in the HTML document
   document.getElementById("root")
-); 
+);
